8a0f7d7737cd32091e444a3f98603c9cd98b8d50
Karen Jonga <karentino2025@yahoo.com>
2025-06-17 06:12:49 +0000

    new changes

---
+++ terraform.tf
@@ -0,0 +1,14 @@
+provider "aws" {
+  version = "~> 5.0"
+  region  = "us-east-1"
+}
+
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.0"
+    }
+  }
+}
+
